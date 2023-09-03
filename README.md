1e46178d433041d1bfca908c6a8f21da92845220
ikagi <macsin777@gmail.com>
2023-09-03 22:32:35 +0200

    Update

--- README.md
+++
@@ -1,2 +0,0 @@
-#VectorV2
-Simple project for small solid motor rocket with live telemetry, tracking and remote pyro channel.
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,195 @@
-#include <iostream>
-#include <asio.hpp>
+#include <cstdio>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <imgui.h>
+#include "imgui-knobs.h"
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
-int main() {
-    asio::io_context io;
-    
-    asio::serial_port serial(io);
-    serial.open("/dev/ttyUSB0");  // Change this to your serial port on Linux
-    serial.set_option(asio::serial_port_base::baud_rate(9600));
-    serial.set_option(asio::serial_port_base::character_size(8));
-    serial.set_option(asio::serial_port_base::parity(asio::serial_port_base::parity::none));
-    serial.set_option(asio::serial_port_base::stop_bits(asio::serial_port_base::stop_bits::one));
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error %d: %s\n", error, description);
+}
 
-    char data;
+void style()
+{
+    ImGuiStyle* style = &ImGui::GetStyle();
 
-    while (true) {
-        asio::read(serial, asio::buffer(&data, 1));
-        std::cout << data; // Process the received data as needed
+    style->Colors[ImGuiCol_Text] = ImVec4(0.86f, 0.93f, 0.89f, 0.78f);
+    style->Colors[ImGuiCol_TextDisabled] = ImVec4(0.86f, 0.93f, 0.89f, 0.28f);
+    style->Colors[ImGuiCol_WindowBg] = ImVec4(0.13f, 0.14f, 0.17f, 1.00f);
+    style->Colors[ImGuiCol_Border] = ImVec4(0.31f, 0.31f, 1.00f, 0.00f);
+    style->Colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    style->Colors[ImGuiCol_FrameBg] = ImVec4(0.20f, 0.22f, 0.27f, 1.00f);
+    style->Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.92f, 0.18f, 0.29f, 0.78f);
+    style->Colors[ImGuiCol_FrameBgActive] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_TitleBg] = ImVec4(0.20f, 0.22f, 0.27f, 1.00f);
+    style->Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.20f, 0.22f, 0.27f, 0.75f);
+    style->Colors[ImGuiCol_TitleBgActive] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_MenuBarBg] = ImVec4(0.20f, 0.22f, 0.27f, 0.47f);
+    style->Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.20f, 0.22f, 0.27f, 1.00f);
+    style->Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.09f, 0.15f, 0.16f, 1.00f);
+    style->Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.92f, 0.18f, 0.29f, 0.78f);
+    style->Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_CheckMark] = ImVec4(0.71f, 0.22f, 0.27f, 1.00f);
+    style->Colors[ImGuiCol_SliderGrab] = ImVec4(0.47f, 0.77f, 0.83f, 0.14f);
+    style->Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_Button] = ImVec4(0.47f, 0.77f, 0.83f, 0.14f);
+    style->Colors[ImGuiCol_ButtonHovered] = ImVec4(0.92f, 0.18f, 0.29f, 0.86f);
+    style->Colors[ImGuiCol_ButtonActive] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_Header] = ImVec4(0.92f, 0.18f, 0.29f, 0.76f);
+    style->Colors[ImGuiCol_HeaderHovered] = ImVec4(0.92f, 0.18f, 0.29f, 0.86f);
+    style->Colors[ImGuiCol_HeaderActive] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_Separator] = ImVec4(0.14f, 0.16f, 0.19f, 1.00f);
+    style->Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.92f, 0.18f, 0.29f, 0.78f);
+    style->Colors[ImGuiCol_SeparatorActive] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_ResizeGrip] = ImVec4(0.47f, 0.77f, 0.83f, 0.04f);
+    style->Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.92f, 0.18f, 0.29f, 0.78f);
+    style->Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_PlotLines] = ImVec4(0.86f, 0.93f, 0.89f, 0.63f);
+    style->Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_PlotHistogram] = ImVec4(0.86f, 0.93f, 0.89f, 0.63f);
+    style->Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.92f, 0.18f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.92f, 0.18f, 0.29f, 0.43f);
+    style->Colors[ImGuiCol_PopupBg] = ImVec4(0.20f, 0.22f, 0.27f, 0.9f);
+}
+
+int main(int, char**)
+{
+    // Setup window
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        return 1;
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1400, 820, "Vector V2 Ground Station", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    glewInit();
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    bool show_test_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImColor(114, 144, 154);
+
+    style(); //Init styles
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClearColor(0.2f, 0.2f, 0.2f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGuiWindowFlags flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove; //Set global window flags
+
+        // feed inputs to dear imgui, start new frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            bool is_open;
+            ImGui::Begin("General settings", &is_open, flags);
+            ImGui::SetWindowSize(ImVec2(400, 200));
+            ImGui::SetWindowPos(ImVec2(5,5));
+            ImGui::Text("Select COM port");
+            ImGui::SameLine();
+            if (ImGui::Button("CONNECT")) {
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("REFRESH")) {
+            }
+            static std::string curr_item = "COM?";
+            ImGui::Text("Average render time: %.0f ms/frame (%.0f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+        {
+            bool is_open;
+
+            ImGui::Begin("Main data", &is_open, flags);
+            ImGui::SetWindowSize(ImVec2(400, 500));
+            ImGui::SetWindowPos(ImVec2(410, 5));
+            ImGui::End();
+        }
+        {
+            bool is_open;
+
+            ImGui::Begin("Pyro Channels", &is_open, flags);
+            ImGui::SetWindowSize(ImVec2(400,300));
+            ImGui::SetWindowPos(ImVec2(410, 510));
+            ImGui::End();
+        }
+        {
+            bool is_open;
+
+            ImGui::Begin("Raw data", &is_open, flags);
+            ImGui::SetWindowSize(ImVec2(400, 600));
+            ImGui::SetWindowPos(ImVec2(5, 210));
+            ImGui::End();
+        }
+        {
+            bool is_open;
+            static float pwr_value = 0;
+
+            ImGui::Begin("Controls", &is_open, flags);
+            ImGui::SetWindowSize(ImVec2(580, 300));
+            ImGui::SetWindowPos(ImVec2(815, 5));
+
+            ImGui::SetCursorPosX(25);
+            ImGui::Text("Eng PWR");
+            ImGui::SetCursorPosX(20);
+            if (ImGui::Button("FULL"))
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    pwr_value++;
+                }
+            }
+            ImGui::SameLine();
+
+            if (ImGui::Button("NULL"))
+            {
+                pwr_value = 0;
+            }
+
+            ImGui::End();
+        }
+
+
+
+
+
+        // Render dear imgui into screen
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
     }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
 
     return 0;
 }
